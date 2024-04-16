4257878c71a74eaa1ae24ebcf1b5dfec59b7fba3
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-04-16 14:20:31 -0300

    Fix: protected increaseMoveCount decreaseMoveCount

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,7 +4,8 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece {
+
 	private Color color;
 	private int moveCount;
 
@@ -15,26 +16,26 @@ public abstract class ChessPiece extends Piece{
 
 	public Color getColor() {
 		return color;
-	} 
-
+	}
+	
 	public int getMoveCount() {
 		return moveCount;
-	} 
-
-	public void increaseMoveCount() {
+	}
+	
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
 
-	public void decreaseMoveCount() {
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
-	
 	}
 }
